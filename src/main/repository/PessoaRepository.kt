cb7567ddb16e8369fd980683244ec3714f49c6a0
liviamoliveira <liviamaria.s.oliveira@hotmail.com>
2021-02-03 21:41:16 -0300

    add finBy

--- src/main/repository/PessoaRepository.kt
+++ src/main/repository/PessoaRepository.kt
@@ -1,10 +1,39 @@
 package main.repository
 
 import main.model.Pessoa
+import kotlin.reflect.jvm.internal.impl.util.ModuleVisibilityHelper
 
 class PessoaRepository {
 
+    private lateinit var listaPessoas: MutableList<Pessoa>
 
+    fun add(pessoa: Pessoa) {
+        listaPessoas.add(pessoa)
+    }
 
+    fun getAll(): List<Pessoa> {
+        return listaPessoas
+    }
 
+    fun getById(id: Int): Pessoa {
+        return listaPessoas.first{ pessoa -> pessoa.id == id }
+    }
+
+    fun getByName(nome: String): List<Pessoa> {
+        return listaPessoas.filter{ pessoa -> pessoa.nome == nome }
+    }
+
+    fun findByCPF(cpf: String): Pessoa {
+        return listaPessoas.first{ pessoa ->  pessoa.cpf == cpf}
+    }
+
+    fun update(pessoa: Pessoa){
+        val posicao = listaPessoas.indexOf(pessoa)
+        listaPessoas[posicao] = pessoa
+    }
+
+    fun delete(id: Int) {
+        val pessoa = getById(id)
+        listaPessoas.remove(pessoa)
+    }
 }
